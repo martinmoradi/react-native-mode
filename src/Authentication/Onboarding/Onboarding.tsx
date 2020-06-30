d6b43755bba2c8fd28c1c0e98b12133d6ef6cca7
William Candillon <wcandillon@gmail.com>
2020-06-30 21:59:52 +0200

    :two:

--- src/Authentication/Onboarding/Onboarding.tsx
+++ src/Authentication/Onboarding/Onboarding.tsx
@@ -1,74 +1,119 @@
-import React from "react";
-import { View, Dimensions, StyleSheet, processColor } from "react-native";
+import React, { useRef } from "react";
+import { View, StyleSheet, Dimensions, Text } from "react-native";
 import { useValue, onScrollEvent, interpolateColor } from "react-native-redash";
-import Animated from "react-native-reanimated";
+import Animated, { multiply } from "react-native-reanimated";
 
-import Slide, { SLIDER_HEIGHT } from "./Slide";
-
+import Slide, { SLIDE_HEIGHT } from "./Slide";
+import Subslide from "./Subslide";
+const BORDER_RADIUS = 75;
 const { width } = Dimensions.get("window");
-const BORDER_RADIUS = 100;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  scrollView: {
-    flex: 1,
-    borderBottomRightRadius: BORDER_RADIUS,
+    backgroundColor: "white",
   },
   slider: {
-    height: SLIDER_HEIGHT,
-    backgroundColor: "white",
+    height: SLIDE_HEIGHT,
+    borderBottomRightRadius: BORDER_RADIUS,
   },
   footer: {
     flex: 1,
+  },
+  footerContent: {
+    flexDirection: "row",
     backgroundColor: "white",
     borderTopLeftRadius: BORDER_RADIUS,
   },
 });
-
 const slides = [
-  { label: "Relaxed", color: "#BFEAF5" },
-  { label: "Playful", color: "#BEECC4" },
-  { label: "Excentric", color: "#FFE4D9" },
-  { label: "Funky", color: "#FFDDDD" },
+  {
+    title: "Relaxed",
+    subtitle: "Find Your Outfits",
+    description:
+      "Confused about your outfit? Don’t worry! Find the best outfit here!",
+    color: "#BFEAF5",
+  },
+  {
+    title: "Playful",
+    subtitle: "Hear it First, Wear it First",
+    description:
+      "Hating the clothes in your wardrobe? Explore hundreds of outfit ideas",
+    color: "#BEECC4",
+  },
+  {
+    title: "Excentric",
+    subtitle: "Your Style, Your Way",
+    description:
+      " Create your individual & unique style and look amazing everyday",
+    color: "#FFE4D9",
+  },
+  {
+    title: "Funky",
+    subtitle: "Look Good, Feel Good",
+    description:
+      "Discover the latest trends in fashion and explore your personality",
+    color: "#FFDDDD",
+  },
 ];
 
 const Onboarding = () => {
+  const scroll = useRef<Animated.ScrollView>(null);
   const x = useValue(0);
+  // TODO: scrollHandler useScrollHandler?
+  const onScroll = onScrollEvent({ x });
   const backgroundColor = interpolateColor(x, {
     inputRange: slides.map((_, i) => i * width),
     outputRange: slides.map((slide) => slide.color),
   });
-  const onScroll = onScrollEvent({ x });
   return (
     <View style={styles.container}>
-      <View style={styles.slider}>
+      <Animated.View style={[styles.slider, { backgroundColor }]}>
+        <Animated.ScrollView
+          ref={scroll}
+          horizontal
+          snapToInterval={width}
+          decelerationRate="fast"
+          showsHorizontalScrollIndicator={false}
+          bounces={false}
+          scrollEventThrottle={1}
+          {...{ onScroll }}
+        >
+          {slides.map(({ title }, index) => (
+            <Slide key={index} right={!!(index % 2)} {...{ title }} />
+          ))}
+        </Animated.ScrollView>
+      </Animated.View>
+      <View style={styles.footer}>
+        <Animated.View
+          style={{ ...StyleSheet.absoluteFillObject, backgroundColor }}
+        />
         <Animated.View
           style={[
-            styles.scrollView,
+            styles.footerContent,
             {
-              backgroundColor,
+              width: width * slides.length,
+              flex: 1,
+              transform: [{ translateX: multiply(x, -1) }],
             },
           ]}
         >
-          <Animated.ScrollView
-            bounces={false}
-            showsHorizontalScrollIndicator={false}
-            snapToInterval={width}
-            decelerationRate="fast"
-            horizontal
-            scrollEventThrottle={1}
-            {...{ onScroll }}
-          >
-            {slides.map(({ label }, i) => (
-              <Slide key={label} label={label} right={!(i % 2)} />
-            ))}
-          </Animated.ScrollView>
+          {slides.map(({ subtitle, description }, index) => (
+            <Subslide
+              key={index}
+              onPress={() => {
+                if (scroll.current) {
+                  console.log({ scrollTo: width * index });
+                  scroll.current
+                    .getNode()
+                    .scrollTo({ x: width * (index + 1), animated: true });
+                }
+              }}
+              last={index === slides.length - 1}
+              {...{ subtitle, description }}
+            />
+          ))}
         </Animated.View>
       </View>
-      <Animated.View style={{ flex: 1, backgroundColor }}>
-        <View style={styles.footer} />
-      </Animated.View>
     </View>
   );
 };
--- src/Authentication/Onboarding/Slide.tsx
+++ src/Authentication/Onboarding/Slide.tsx
@@ -1,43 +1,39 @@
 import React from "react";
-import { View, Text, StyleSheet, Dimensions } from "react-native";
+import { View, Text, Dimensions, StyleSheet } from "react-native";
 
-const { height, width } = Dimensions.get("window");
-export const SLIDER_HEIGHT = height * 0.61;
-
+const { width, height } = Dimensions.get("window");
+export const SLIDE_HEIGHT = 0.61 * height;
 const styles = StyleSheet.create({
   container: {
     width,
   },
+  titleContainer: {
+    height: 100,
+    justifyContent: "center",
+  },
   title: {
-    fontFamily: "SFProText-Bold",
     fontSize: 80,
     lineHeight: 80,
+    fontFamily: "SFProText-Bold",
     color: "white",
     textAlign: "center",
   },
 });
-
 interface SlideProps {
-  label: string;
+  title: string;
   right?: boolean;
 }
 
-const Slide = ({ label, right }: SlideProps) => {
+const Slide = ({ title, right }: SlideProps) => {
   const transform = [
-    { translateX: -height / 4 },
+    { translateY: (SLIDE_HEIGHT - 100) / 2 },
+    { translateX: right ? width / 2 - 50 : -width / 2 + 50 },
     { rotate: right ? "-90deg" : "90deg" },
-    { translateX: right ? -height / 4 : height / 4 },
-    { translateY: right ? width - 80 - 24 : 0 },
   ];
   return (
     <View style={styles.container}>
-      <View
-        style={{
-          width: SLIDER_HEIGHT,
-          transform,
-        }}
-      >
-        <Text style={styles.title}>{label}</Text>
+      <View style={[styles.titleContainer, { transform }]}>
+        <Text style={styles.title}>{title}</Text>
       </View>
     </View>
   );
